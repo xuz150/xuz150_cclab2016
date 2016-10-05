3980531d7a60af9a608d717e6bc0e3f7acada9dc
Zhiyang Xu <xuz150@newschool.edu>
2016-10-04 22:05:45 -0400

    Updating..

---
+++ Week 5/Week_5/Week_5.ino
@@ -0,0 +1,17 @@
+int ledPin = 9;
+int brightness = 0;
+int sensorLow = 0;
+int sensorHigh = 100;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop(){
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  brightness = map(sensorValue, sensorLow, sensorHigh, 0, 255);
+  analogWrite(ledPin, brightness);
+  delay(300);
+}
